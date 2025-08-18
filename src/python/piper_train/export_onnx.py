ce0f0798beb464aef30724f8ea893b01b2c371c3
Flávio De Melo <flavio.eler@gmail.com>
2025-08-18 18:11:12 +0200

    Fix export

--- src/python/piper_train/export_onnx.py
+++ src/python/piper_train/export_onnx.py
@@ -94,10 +94,10 @@ def main() -> None:
     scales = torch.FloatTensor([0.667, 1.0, 0.8])
     dummy_input = (sequences, sequence_lengths, scales, sid)
 
-    batch_size = torch.export.Dim.AUTO
-    phone_seq_len = torch.export.Dim.AUTO
-    scale_size = torch.export.Dim.AUTO
-    # time_seq_len = torch.export.Dim.AUTO
+    batch_size = torch.export.Dim.DYNAMIC
+    phone_seq_len = torch.export.Dim.DYNAMIC
+    scale_size = torch.export.Dim("scale_size", min=3)
+    # time_seq_len = torch.export.Dim("time_seq_len", min=1)
 
     # Export
     torch.onnx.export(
